ae3f0effc6ca5805d761f07c2b258fa24f0f21f5
Amrom Steinmetz <amroms@gmail.com>
2016-11-14 10:52:58 -0500

    tables synced

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var nunjucks = require('nunjucks');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var path = require('path');
+var models = require('./models');
 
 var env = nunjucks.configure('views', {noCache: true});
 
@@ -20,6 +21,15 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(router);
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+models.User.sync({})
+.then(function() {
+  models.Page.sync({})
 })
+.then(function() {
+  app.listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
+})
+.catch(console.error);
+
+
--- models/index.js
+++ models/index.js
@@ -1,2 +1,34 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack');
+
+var Page = db.define('page',{
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  urlTitle: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.STRING,
+    defaultValue: null
+  }
+});
+
+var User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = {Page, User};
